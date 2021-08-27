9fb64ab80ff60b21be5324fe8925ef5031ec8317
tdcas <allanf333@gmail.com>
2021-08-27 00:41:16 -0300

    segundo commit

--- lib/Controller/controlador.dart
+++ lib/Controller/controlador.dart
@@ -1,3 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:nosso_app/View/pconversor.dart';
+
 class Controlador {
-  
+  TextEditingController campo1 = new TextEditingController(text: "1");
+  TextEditingController campo2 = new TextEditingController(text: "");
+  num resultado;
+
+  num calcular() {
+    num real = 0;
+    num dolar = 0;
+    resultado = real - dolar;
+    return resultado;
+  }
+
+  void setState(Null Function() param0) {}
 }
--- lib/View/pconversor.dart
+++ lib/View/pconversor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nosso_app/Controller/controlador.dart';
 
 class Pconversor extends StatefulWidget {
   @override
@@ -9,7 +10,7 @@ class PconversorState extends State<Pconversor> {
   List<String> listinha = ["Dolar", "Real", "Peso"];
   List<int> numero = [0, 1];
   var dropdownValue = 0;
-
+  Controlador control = new Controlador();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,31 +19,23 @@ class PconversorState extends State<Pconversor> {
       ),
       body: new Column(children: [
         new Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(horizontal: 25, vertical: 175),
+            alignment: Alignment.bottomLeft,
+            margin: EdgeInsets.symmetric(horizontal: 25, vertical: 130),
             color: Colors.grey[800],
             child: new Column(
               children: [
-                new TextField(
-                    //Controller: ,
-                    decoration: InputDecoration(
-                        labelText: "Real",
-                        labelStyle:
-                            TextStyle(color: Colors.white, fontSize: 19),
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey)),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black)))),
-                SizedBox(height: 20, width: 50),
+                //Menu de escolhas 1
                 new DropdownButton(
-                  value: dropdownValue,
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                  isExpanded: true,
+                  value: 1,
                   items: [
                     DropdownMenuItem(
-                      child: Text(listinha[numero[0]]),
+                      child: Text(listinha[0]),
                       value: numero[0],
                     ),
                     DropdownMenuItem(
-                      child: Text(listinha[numero[1]]),
+                      child: Text(listinha[1]),
                       value: numero[1],
                     )
                   ],
@@ -52,28 +45,51 @@ class PconversorState extends State<Pconversor> {
                     });
                   },
                 ),
+                //barra de digitaçao 1
                 new TextField(
-                    //controller: ,
+                    controller: control.campo1,
                     decoration: InputDecoration(
-                        labelText: "Dolar",
-                        labelStyle:
-                            TextStyle(color: Colors.white, fontSize: 19),
                         border: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.grey)),
                         focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.black)))),
+                //Menu de escolhas
 
-                /*new Container(
-                  child: new DropdownButton()
-                  
-                )*/
+                new DropdownButton(
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                  value: 0,
+                  items: [
+                    DropdownMenuItem(
+                      child: Text(listinha[0]),
+                      value: numero[0],
+                    ),
+                    DropdownMenuItem(
+                      child: Text(listinha[1]),
+                      value: numero[1],
+                    )
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      value = value;
+                    });
+                  },
+                ),
+                new SizedBox(height: 20),
+                new Text("Resultado ${control.calcular}",
+                    style: TextStyle(fontSize: 28)),
+                new SizedBox(height: 20),
+                //Botao CONVERTER
+                new MaterialButton(
+                  padding: EdgeInsets.all(20),
+                  child: new Text("Converter",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  color: Colors.yellow,
+                  onPressed: control.calcular,
+                )
               ],
             )),
       ]),
     );
   }
-}
-
-widgetTextField() {
-  return TextField();
 }
